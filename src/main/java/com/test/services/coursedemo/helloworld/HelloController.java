eb33f8d9f20ccbfeb1cb22b402890c345d8a69d1
Neetin Nagap <nnagap72@gmail.com>
2020-10-03 15:29:21 +0530

    Adding simple get mapping instead of req mapping

--- src/main/java/com/test/services/coursedemo/helloworld/HelloController.java
+++ src/main/java/com/test/services/coursedemo/helloworld/HelloController.java
@@ -1,5 +1,6 @@
 package com.test.services.coursedemo.helloworld;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +10,11 @@ public class HelloController {
 
     @RequestMapping(method = RequestMethod.GET, path = "/helloworld")
     public String helloWorld() {
+        return "Hello World!";
+    }
+
+    @GetMapping(path = "/helloworld2")
+    public String helloWorldDirectMapping() {
         return "Hello World!";
     }
 }
